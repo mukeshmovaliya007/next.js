395537d94d9c95fd68ad6237c837e46003aaa332
mukeshmovaliya007 <38279147+mukeshmovaliya007@users.noreply.github.com>
2024-11-18 18:04:20 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
 'use client'
+import Link from "next/link";
 import styles from "./page.module.css";
 export default function Home() {  
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Demo</h1>
+       <Link href="/login"> Go TO Login Page</Link>
+       <br />
+       <br />
+       <Link href="/about"> Go TO About Us Page</Link>       
       </main>     
     </div>
   );
